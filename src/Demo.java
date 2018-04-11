ebea6a20b3b6c9c6e74db697c86a2f7c9f1f7ba1
unknown <ivelin.mladenov@yahoo.com>
2018-04-11 11:31:50 +0300

    adding some demo

--- src/Demo.java
+++ src/Demo.java
@@ -1,28 +1,60 @@
+import java.util.Scanner;
+
 import com.technomarket.products.Product;
 import com.technomarket.products.SearchBar;
+import com.technomarket.users.User;
 import com.technomarket.users.UserException;
 
 public class Demo {
 
 	public static void main(String[] args) throws UserException {
-
-		// Product.showAllProducts();
-//		ITproducts.generateITproducts();
-//		ITproducts.Laptop.showLaptops();
-//		ITproducts.MobilePhone.showMobilePhones();
-//		SearchBar.searchByKeyword();
-//		
-//		Product.generateProducts();
-//		Product.showAllProducts();
-
-//		
-//		Product.generateProducts();
-//		Product.showAllProducts();
-//		
-//		SearchBar.searchByPrice();
-
 		
 
+		System.out.println("----------------Technomarket----------------");
+		System.out.println();
+		System.out.println("\tDobre doshli v glavnoto MENU");
+		System.out.println("-------------------------------");
+		System.out.println("1. Za da razgledate vsichki produkti >>> show all");
+		System.out.println("2. Za da napravite registraciq >>> register");
+		System.out.println("3. Za da se lognete v saita >>> login");
+		System.out.println("4. Za da se otpishte ot saite >>> logout");
+		System.out.println("5. Za informaciq otnostno saita >>> info");
 		
+		
+		while (true) {
+			
+			
+			Scanner sc = new Scanner(System.in);
+			String choice = sc.nextLine().toLowerCase();
+			
+			switch (choice) {
+			
+			case "show all":
+				SearchBar.printCatalog();
+				break;
+			case "register":
+				User.generateUser();
+				break;
+			case "login":
+				try {
+					User.login();
+				} catch (Exception e) {
+					System.out.println("Cannot login right now! Try again later!");
+				}
+				break;
+			case "logout":
+//				User.logout();
+				break;
+			case "info":
+				System.out.println("Technomarket - sait prednaznachen za elektronni ustroistva");
+				break;
+
+			default: System.out.println("Greshna komanda !");
+				break;
+			}
+			
+			
+			
+		}
 	}
 }
--- src/com/technomarket/users/DemoUser.java
+++ src/com/technomarket/users/DemoUser.java
@@ -5,23 +5,23 @@ import com.technomarket.products.SearchBar;
 
 public class DemoUser {
 	public static void main(String[] args) throws UserException, OrderException {
-		
-		User user = User.generateUser();
-		try {
-			User.login();
-		} catch (Exception e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		}
-
-		try {
-			SearchBar.readJsonFile();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-
-		SearchBar.createJsonFile();
-		SearchBar.searchByPrice();
+//		
+//		User user = User.generateUser();
+//		try {
+//			User.login();
+//		} catch (Exception e1) {
+//			// TODO Auto-generated catch block
+//			e1.printStackTrace();
+//		}
+//
+//		try {
+//			SearchBar.readJsonFile();
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
+//
+//		SearchBar.createJsonFile();
+//		SearchBar.searchByPrice();
 
 		
 	}
